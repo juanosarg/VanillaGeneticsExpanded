2f1dd69f62c8c1ac5fac4267b0bdccb9418f4c15
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-03-18 20:04:38 +0100

    v0.1

--- 1.2/Source/GeneticRim/GeneticRim/Defs/DraftableAnimalDef.cs
+++ 1.2/Source/GeneticRim/GeneticRim/Defs/DraftableAnimalDef.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RimWorld;
+using Verse;
 
-namespace GeneticRim.Defs
+namespace GeneticRim
 {
-    class DraftableAnimalDef
+    public class DraftableAnimalDef : Def
     {
+
+        //DraftableAnimalDef is a simple custom def that allows you to input a list of defNames for use
+        //in Genetic Rim's custom animal drafting
+
+        //A list of pawnkind defNames
+        public List<string> draftablePawnDefs;
     }
 }
