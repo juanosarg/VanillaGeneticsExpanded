4bc357091263c057bb7b4a9b760eda3b51af28d7
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-02-25 15:14:07 +0100

    Random hybrid spawners

---
+++ 1.3/Source/GeneticRim/GeneticRim/Comps/CompProperties/CompProperties_RandomHybridSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticRim
+{
+    using Verse;
+
+    public class CompProperties_RandomHybridSpawner : CompProperties
+    {
+       
+
+        public CompProperties_RandomHybridSpawner()
+        {
+            this.compClass = typeof(CompRandomHybridSpawner);
+        }
+    }
+}
---
+++ 1.3/Source/GeneticRim/GeneticRim/Comps/CompRandomHybridSpawner.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using Verse;
+namespace GeneticRim
+{
+
+
+
+    public class CompRandomHybridSpawner : ThingComp
+    {
+        public CompProperties_RandomHybridSpawner Props => (CompProperties_RandomHybridSpawner)this.props;
+
+        public HashSet<PawnKindDef> hybridsList = new HashSet<PawnKindDef>();
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+           hybridsList = DefDatabase<PawnKindDef>.AllDefsListForReading.Where(x => (x.race.tradeTags?.Contains("AnimalGenetic") == true)&&x.defName!= "GR_ArchotechCentipede").ToHashSet();
+
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (this.parent.IsHashIntervalTick(500))
+            {
+
+                int num = GenRadial.NumCellsInRadius(12);
+                for (int i = 0; i < num; i++)
+                {
+                    IntVec3 current = this.parent.Position + GenRadial.RadialPattern[i];
+                    if (current.InBounds(this.parent.Map))
+                    {
+                        Pawn pawn = current.GetFirstPawn(this.parent.Map);
+                        if ((pawn != null) && (pawn.Faction==Faction.OfPlayer))
+                        {
+                            SpawnHostileHYbrid();
+                            break;
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+        public void SpawnHostileHYbrid()
+        {
+            Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(hybridsList.RandomElement(), null, fixedBiologicalAge: 3, fixedChronologicalAge: 3,
+                                                                                       newborn: false, forceGenerateNewPawn: true));
+            IntVec3 near = CellFinder.StandableCellNear(this.parent.Position, this.parent.Map, 2f);
+
+            GenSpawn.Spawn(pawn, near, this.parent.Map);
+
+            pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent, null, true, false, null, false);
+
+            this.parent.Destroy();
+            
+        }
+
+    }
+}
