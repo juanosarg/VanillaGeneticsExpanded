42a12088e687514a5ba92ed284375b839713f6c0
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-03-25 18:34:46 +0100

    Megafauna module

---
+++ 1.3/Source/GeneticRim/GeneticRim/Abilities/CompJump.cs
@@ -0,0 +1,42 @@
+﻿
+using Verse;
+using System;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+
+
+
+
+namespace GeneticRim
+{
+    class CompJump : CompAbilityEffect
+    {
+
+        new public CompProperties_Jump Props
+        {
+            get
+            {
+                return (CompProperties_Jump)this.props;
+            }
+        }
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            Map map = parent.pawn.Map;
+            PawnFlyer pawnFlyer = PawnFlyer.MakeFlyer(ThingDefOf.PawnJumper, parent.pawn, target.Cell);
+            if (pawnFlyer != null)
+            {
+                GenSpawn.Spawn(pawnFlyer, target.Cell, map);
+              
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ 1.3/Source/GeneticRim/GeneticRim/Abilities/Properties/CompProperties_Jump.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GeneticRim
+{
+
+
+    public class CompProperties_Jump : CompProperties_AbilityEffect
+    {
+
+       
+
+
+        public CompProperties_Jump()
+        {
+            this.compClass = typeof(CompJump);
+        }
+    }
+}
