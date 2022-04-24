ce9220c3efd6b8910e1c9219779dd1ac5b27032a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-04-24 12:43:48 +0200

    RimBees patch

---
+++ 1.3/Source/GeneticRim/GeneticRim/DeathActionWorkers/DeathActionWorker_BeeExplosion.cs
@@ -0,0 +1,41 @@
+﻿
+using RimWorld;
+using Verse;
+
+
+namespace GeneticRim
+{
+    public class DeathActionWorker_BeeExplosion : DeathActionWorker
+    {
+
+
+
+        public override void PawnDied(Corpse corpse)
+        {
+            float radius;
+            if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0)
+            {
+                radius = 1.9f;
+            }
+            else if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 1)
+            {
+                radius = 2.9f;
+            }
+            else
+            {
+                radius = 3.9f;
+            }
+
+
+
+            GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, Named("BR_BeeExplosion"), corpse.InnerPawn, 1, -1, SoundDef.Named("Explosion_Bomb"), null, null, null, ThingDef.Named("GR_InsectCloudMotes"), 1f, 1, false, null, 0f, 1);
+        }
+
+        public static DamageDef Named(string defName)
+        {
+            return DefDatabase<DamageDef>.GetNamed(defName, true);
+        }
+
+
+    }
+}
