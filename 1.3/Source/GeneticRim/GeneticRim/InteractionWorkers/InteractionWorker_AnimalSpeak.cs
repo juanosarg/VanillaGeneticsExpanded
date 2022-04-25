2a04fdec42fd974983083e7391f4916f7aeae844
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-04-25 16:31:23 +0200

    Fixed interaction workers

---
+++ 1.3/Source/GeneticRim/GeneticRim/InteractionWorkers/InteractionWorker_AnimalSpeak.cs
@@ -0,0 +1,20 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace GeneticRim
+{
+    public class InteractionWorker_AnimalSpeak : InteractionWorker
+    {
+        public override float RandomSelectionWeight(Pawn initiator, Pawn recipient)
+        {
+            if (StaticCollectionsClass.IsHumanoidHybrid(initiator) && recipient.RaceProps.Humanlike)
+            {
+                return 1f;
+            }
+            else return 0;
+
+
+        }
+    }
+}
---
+++ 1.3/Source/GeneticRim/GeneticRim/InteractionWorkers/InteractionWorker_UWUSpeak.cs
@@ -0,0 +1,20 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace GeneticRim
+{
+    public class InteractionWorker_UWUSpeak : InteractionWorker
+    {
+        public override float RandomSelectionWeight(Pawn initiator, Pawn recipient)
+        {
+            if (initiator.def==InternalDefOf.GR_Mancat && recipient.RaceProps.Humanlike)
+            {
+                return 1f;
+            }
+            else return 0;
+
+
+        }
+    }
+}
